95100ce5e0ff339e3d19f5dba72108e32616e238
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-09-11 15:10:37 +0900

    깜빡임 애니메이션 추가

--- app/src/main/java/com/example/animationex/MainActivity.kt
+++ app/src/main/java/com/example/animationex/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.animationex
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import android.view.animation.RotateAnimation
-import android.widget.TextView
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.animationex.anim.RotateExpandAnimation
 import com.example.animationex.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var isRotate = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +31,34 @@ class MainActivity : AppCompatActivity() {
                 targetExpandView = binding.layoutSampleTxt
             )
         }
+
+        //val anim
+        val anim = AnimationUtils.loadAnimation(this, R.anim.blink_animation)
+        binding.ivAndroid.startAnimation(anim)
+        
+//        binding.ivAndroid.setOnClickListener {
+//            val animation = if (isRotate) {
+//                ObjectAnimator.ofFloat(it, "rotationY", 0.0f, 180f)
+//            } else {
+//                ObjectAnimator.ofFloat(it, "rotationY", 180f, 0.0f)
+//            }
+//            isRotate = !isRotate
+//
+//
+//            animation.duration = 200
+//            animation.repeatCount = 1
+//            animation.interpolator = AccelerateDecelerateInterpolator()
+//            animation.start()
+//        }
+//
+//        val animation =  ObjectAnimator.ofFloat(binding.ivAndroid, "rotationY", 90f, 0.0f)
+//        animation.duration = 200
+//        animation.repeatCount = 1
+//        animation.interpolator = AccelerateDecelerateInterpolator()
+//        animation.start()
+
+//        binding.ivAndroid.setOnClickListener {
+//            it.animate().rotationX(180f)
+//        }
     }
 }
