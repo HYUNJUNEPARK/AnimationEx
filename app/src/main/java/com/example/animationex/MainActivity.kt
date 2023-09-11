f27c190c83d99f7602765641095fcd154a3ff17d
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-09-11 15:47:31 +0900

    회전 -> 이미지 변경 애니메이션 적용

--- app/src/main/java/com/example/animationex/MainActivity.kt
+++ app/src/main/java/com/example/animationex/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.animationex
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -15,7 +15,8 @@ import com.example.animationex.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private var isRotate = true
+    private var isTest = true
+    private var isTest2 = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +33,50 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        //val anim
+        //Blink
         val anim = AnimationUtils.loadAnimation(this, R.anim.blink_animation)
         binding.ivAndroid.startAnimation(anim)
-        
+
+
+        //
+        binding.ivAir.setOnClickListener { view ->
+            var animation =  ObjectAnimator.ofFloat(view, "rotationY", 0.0f, 90f)
+
+            animation.duration = 200
+            animation.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    super.onAnimationEnd(animation)
+
+                    val imageRes = if (isTest) R.drawable.ic_android_red_24dp else R.drawable.ic_android_black_24dp
+                    view.setBackgroundResource(imageRes)
+                    isTest = !isTest
+
+                    var animation = ObjectAnimator.ofFloat(view, "rotationY", 90f, 180f)
+
+                    animation.duration = 200
+                    animation.start()
+
+                }
+            })
+            animation.start()
+
+
+//            //https://copycoding.tistory.com/114
+//            val rotate = RotateAnimation(
+//                0f,
+//                180f,
+//                Animation.RELATIVE_TO_SELF,
+//                0.5f,
+//                Animation.RELATIVE_TO_SELF,
+//                0.5f
+//            )
+//            rotate.duration = 200
+//            rotate.interpolator = LinearInterpolator()
+//
+//            binding.ivAir.startAnimation(rotate)
+        }
+
+
 //        binding.ivAndroid.setOnClickListener {
 //            val animation = if (isRotate) {
 //                ObjectAnimator.ofFloat(it, "rotationY", 0.0f, 180f)
@@ -51,14 +92,15 @@ class MainActivity : AppCompatActivity() {
 //            animation.start()
 //        }
 //
-//        val animation =  ObjectAnimator.ofFloat(binding.ivAndroid, "rotationY", 90f, 0.0f)
-//        animation.duration = 200
-//        animation.repeatCount = 1
-//        animation.interpolator = AccelerateDecelerateInterpolator()
-//        animation.start()
+
 
 //        binding.ivAndroid.setOnClickListener {
 //            it.animate().rotationX(180f)
 //        }
     }
+
+    companion object {
+        const val TAG = "testLog"
+    }
+
 }
